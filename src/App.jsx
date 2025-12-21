bf3a2a5373d14439862223d454f1e13fa4013ff1
hellsten <jessicahellsten@live.com>
2025-12-21 13:46:34 -0800

    Adding attending event banner and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.scss";
 import PageHeader from "./components/PageHeader/PageHeader.jsx";
 import EventList from "./components/EventList/EventList.jsx"
 import ComponentDocs from "./pages/ComponentDocs/ComponentDocs.jsx";
+import PageFooter from "./components/PageFooter/PageFooter.jsx";
+import AttendingCard from "./components/AttendingCard/AttendingCard.jsx";
 
 const App = () => {
   return (
@@ -10,7 +12,19 @@ const App = () => {
       <PageHeader title="Events">
         <EventList />
       </PageHeader>
+      
+      <div className="attending-banner">
+        <AttendingCard 
+        iconName="calendar"
+        cardTitle="Attending an event?"
+        description="Stay organized and in the loop. Check your upcoming event registrations and view schedules, updates, and important info. Everything you need to know is all in one place."
+        buttonText="View your events"
+        buttonLink="#"
+        />
+      </div>
+      <PageFooter />
       <ComponentDocs />
+
     </>
   );
 };
